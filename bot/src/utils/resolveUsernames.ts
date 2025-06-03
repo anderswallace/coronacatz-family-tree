190c327c668d6e34f3feeddc7bcbc8ea8f549309
awallace2 <anderswallace20@gmail.com>
2025-06-03 17:50:36 -0500

    docs: add documentation for resolveUsernames

--- bot/src/utils/resolveUsernames.ts
+++ bot/src/utils/resolveUsernames.ts
@@ -1,5 +1,16 @@
-import { GuildMember, Message } from "discord.js";
+import { Message } from "discord.js";
 
+/**
+ * Resolves the display names from two User IDs and Discord message
+ *
+ * If message is a DM (Direct Message), or the users are not found in the server
+ * it returns null
+ *
+ * @param message
+ * @param childId
+ * @param parentId
+ * @returns The display name of the child and parent User IDs
+ */
 export async function resolveUsernames(
   message: Message,
   childId: string,
@@ -18,6 +29,14 @@ export async function resolveUsernames(
   return { childUsername, parentUsername };
 }
 
+/**
+ * Utility to extract a GuildMember (Server specific user reference) from a Discord server
+ * message using a User ID (Global user reference)
+ *
+ * @param message
+ * @param userId
+ * @returns GuildMember from server with matching {@link userId}
+ */
 async function fetchGuildMember(message: Message, userId: string) {
   // verify message isn't a private message
   const guild = message.guild;
