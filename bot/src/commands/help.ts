be281803be758c5e6fe7bbe32ef9af724978d6cf
awallace2 <anderswallace20@gmail.com>
2025-06-03 19:11:32 -0500

    docs: add documentation for handleHelpCommand

--- bot/src/commands/help.ts
+++ bot/src/commands/help.ts
@@ -8,8 +8,16 @@ export const helpCommand = new SlashCommandBuilder()
   .setName("help")
   .setDescription("Shows how to interact with the family tree bot");
 
+/**
+ * Slash command to handle /help interaction with the bot
+ *
+ * Creates an ephemeral reply in the channel with instructions on
+ * how to use the bot
+ *
+ * @param interaction
+ */
 export async function handleHelpCommand(
-  interaction: ChatInputCommandInteraction
+  interaction: ChatInputCommandInteraction,
 ) {
   const helpMessage = `
    **Family Tree Bot Help**
